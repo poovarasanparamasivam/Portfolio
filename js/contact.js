b2c1eab8cda8f24a8c775a9132b37e17921c0174
poovarasanparamasivam <poovarasanparamasivam@outlook.com>
2024-01-28 06:50:37 -0800

    all code changed

--- js/contact.js
+++ js/contact.js
@@ -10,27 +10,49 @@ function submit(){
         document.getElementById('fnameerror').innerText='Please Enter the valid name';
         return;
     }
+    else{
+        document.getElementById('fnameerror').innerText='';
+    }
     if(lastname.trim()===''){
         document.getElementById('lnameerror').innerText='Please Enter the valid name';
         return;
+    }
+    else{
+        document.getElementById('fnameerror').innerText='';
     }
     if(email.trim()===''){
         document.getElementById('mailerror').innerText='Please Enter the valid email';
         return;
     }
+    else{
+        document.getElementById('fnameerror').innerText='';
+    }
     if(mobile.trim()===''){
         document.getElementById('mobilenumbererror').innerText='Please Enter the valid mobile number';
         return;
+    }
+    else{
+        document.getElementById('fnameerror').innerText='';
     }
     if(address.trim()===''){
         document.getElementById('addresserror').innerText='Please Enter the valid address';
         return;
     }
+    else{
+        document.getElementById('fnameerror').innerText='';
+    }
     if(summary.trim()===''){
         document.getElementById('summaryerror').innerText='Please Enter the valid summary';
         return;
     }
+    else{
+        document.getElementById('fnameerror').innerText='';
+    }
 
     alert("All data's are Submitted successfully. Our team will contact you soon!");
+    setTimeout(function(){
+        location.reload();
+
+    },100);
 
 }
